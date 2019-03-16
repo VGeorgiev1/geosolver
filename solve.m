47e31302e7ef4e9b7ab6794c8ce6527a393c27a4
mdatsev <martin.datsev.p@gmail.com>
2019-03-16 02:09:03 +0200

    added fullSimlify

--- solve.m
+++ solve.m
@@ -2,7 +2,9 @@ eqs = ToExpression[$ScriptCommandLine[[2]]]
 
 solved = Solve[eqs, answer, MaxExtraConditions -> All]
 (* Write[Streams["stderr"], solved] *)
-extracted = solved /. ConditionalExpression[e_, _] :> ConditionalExpression[e, True]
+simplified = FullSimplify[solved]
+
+extracted = simplified /. ConditionalExpression[e_, _] :> ConditionalExpression[e, True]
 
 extracted2 = answer /. extracted
 
