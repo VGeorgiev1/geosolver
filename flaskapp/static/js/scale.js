b7b246b0563ddea42e5768060f79ddf92820a563
VGeorgiev1 <vladdislav.georgiev@gmail.com>
2019-03-16 15:05:25 +0200

    zomming

--- flaskapp/static/js/scale.js
+++ flaskapp/static/js/scale.js
@@ -1,6 +1,6 @@
 let width = 720;
 let height = 720;
-let scale_z0 = 40;
+let scale_z = 40;
 let mx=0;
 let my=0;
 let wheelCount = 0;
@@ -10,9 +10,8 @@ let scaleFactor = 1.0;
 let translateX = 0.0;
 let translateY = 0.0;
 let zoomSensitivity = 0.001
-let fontSize = 20;
+let fontSize = 10;
 let strWeight = 1;
-let rescale = false 
 function setup(){
     createCanvas(width,height);
 }
@@ -26,60 +25,63 @@ function mouseDragged() {
 }
 function mouseWheel(event) {
     event.preventDefault();
-    if(event.delta > 0){
-       rescale = true
-    }
     let delta = 1 - event.delta * zoomSensitivity;
     if((scaleFactor * delta) >1){
         scaleFactor*=delta
-       
         translateX -= mouseX;
         translateY -= mouseY;
         translateX *= delta;
         translateY *= delta;
         translateX += mouseX;
         translateY += mouseY;
-        
+    }else{
+        scaleFactor = 1.0
     }
     return false;
 }
-function calculate() {
-    console.log(translateX * scaleFactor)
-}
-function draw(){
-    background(255)
-    translate(translateX, translateY);
-    scale(scaleFactor);
-    const scale_z = scale_z0 // scaleFactor
-    let curHeight = 0;
-    let curWidth = 0;
-    let stepWidth = width / (scale_z );
-    let stepHeight = height / (scale_z );
+function drawObjects(stepWidth, stepHeight){
     for(let i=0;i<figures.length;i++){
         stroke(0)
         strokeWeight(strWeight / scaleFactor)
         line(figures[i].x1*stepWidth+360,-figures[i].y1*stepHeight+360,figures[i].x2*stepWidth+360,-figures[i].y2*stepHeight+360)
     }
+}
+function drawLabels(step){
+    let curY = 0;
+    for(let i=(width/step)/2; i>-width/step/2;i--){
+        textSize(fontSize/scaleFactor)
+        text(i, width/2, curY);
+        curY+=step;
+    }
+    let curX = 0;
+    for(let i=-(width/step)/2; i<width/step/2;i++){
+        textSize(fontSize/scaleFactor)
+        text(i, curX, height/2);
+        curX+=step;
+    }
+}
+let old_factor = scaleFactor
+function draw(){
+    background(255)
+    translate(translateX, translateY);
+    scale(scaleFactor);
+    let curHeight = 0;
+    let curWidth = 0;
+    if(scaleFactor > old_factor * 2){
+        old_factor = scaleFactor  
+    }else if(old_factor > scaleFactor * 2){
+        old_factor = scaleFactor
+    }
     
-    let c = color(0, 126, 255);
-    for(let i=-scale_z/2;i<scale_z/2;i++){
-        i==0?stroke(0):stroke(125);
+    let step = (Math.ceil(width / (scale_z * old_factor)))
+
+    for(let i=0;i<Math.ceil(width / step);i++){
+        i==Math.floor(Math.floor(width / step)/2)?stroke(0):stroke(200);
         strokeWeight(strWeight / scaleFactor)
         line(0,curHeight, width,curHeight)
         line(curWidth,0, curWidth,height)
-        curHeight+=stepHeight;
-        curWidth+=stepWidth;
+        curHeight+=step;
+        curWidth+=step;
     }
-    let curY = 0;
-    for(let l=-scale_z/2;l<=scale_z/2;l++){
-        textSize(fontSize/scaleFactor)
-        text(-l, curWidth/2, curY);
-        curY+=stepHeight;
-    }
-    let curX = 0;
-    for(let l=-(scale_z/2);l<=scale_z/2;l++){
-        textSize(fontSize/scaleFactor)
-        text(l, curX, curHeight/2);
-        curX+=stepWidth;
-    }
+    drawLabels(step)
 }
